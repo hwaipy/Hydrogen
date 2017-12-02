604c5d0e8ecc623c796f1487577f439103de1391
Hwaipy Li <hwaipy@gmail.com>
2017-12-03 00:41:22 +0800

    add Permutation repeatable mode

--- src/main/scala/com/hwaipy/hydrogen/math/Permutation.scala
+++ src/main/scala/com/hwaipy/hydrogen/math/Permutation.scala
@@ -4,11 +4,16 @@ import java.util.concurrent.atomic.AtomicReference
 import java.util.Arrays
 
 object Permutation {
-  def iterator(size: Int, max: Int, min: Int = 1) = new PermutationIterable(size, max, min).iterator
+  //if not repeatable, returns (1,2,3),(1,2,4),(1,2,5),...
+  //if repeatable, returns (1,1,1),(1,1,2),(1,1,3),...
+  def iterator(size: Int, max: Int, min: Int = 1, repeatable: Boolean = false) = new PermutationIterable(size, max, min, repeatable).iterator
 }
 
-private class PermutationIterable(size: Int, private val max: Int, private val min: Int = 1) extends Iterable[List[Int]] {
-  private val initial = Range(0, size).map(a => min + a).toList
+private class PermutationIterable(size: Int, private val max: Int, private val min: Int = 1, repeatable: Boolean = false) extends Iterable[List[Int]] {
+  private val initial = repeatable match {
+    case true => Range(0, size).map(a => min).toList
+    case false => Range(0, size).map(a => min + a).toList
+  }
 
   def iterator: Iterator[List[Int]] = new ArrangerIterator
 
@@ -20,11 +25,14 @@ private class PermutationIterable(size: Int, private val max: Int, private val m
     def next = {
       if (!hasNext) throw new IllegalStateException("No more item.")
       val ret = nextItem.get.get
-      loadNext
+      repeatable match {
+        case true => loadNextRepeatable
+        case false => loadNextUnrepeatable
+      }
       ret
     }
 
-    private def loadNext = {
+    private def loadNextUnrepeatable = {
       val current = nextItem.get.get.toArray
       current(current.size - 1) += 1
       var carryI = current.size
@@ -36,6 +44,27 @@ private class PermutationIterable(size: Int, private val max: Int, private val m
       })
       Range(carryI, current.size).foreach(i => {
         current(i) = current(i - 1) + 1
+      })
+      if (current(current.size - 1) > PermutationIterable.this.max) {
+        nextItem.set(None)
+      }
+      else {
+        nextItem.set(Option(current.toList))
+      }
+    }
+
+    private def loadNextRepeatable = {
+      val current = nextItem.get.get.toArray
+      current(current.size - 1) += 1
+      var carryI = current.size
+      Range(current.size - 1, 0, -1).foreach(i => {
+        if (current(i) > PermutationIterable.this.max) {
+          current(i - 1) += 1
+          carryI = i
+        }
+      })
+      Range(carryI, current.size).foreach(i => {
+        current(i) = current(i - 1)
       })
       if (current(current.size - 1) > PermutationIterable.this.max) {
         nextItem.set(None)
--- src/test/scala/com/hwaipy/hydrogen/math/PermutationTest.scala
+++ src/test/scala/com/hwaipy/hydrogen/math/PermutationTest.scala
@@ -17,7 +17,7 @@ class PermutationTest extends FunSuite with BeforeAndAfter with BeforeAndAfterAl
   }
 
   test("Test PermutationIterator.") {
-    val exp = List(List(2, 3, 4, 5), List(2, 3, 4, 6), List(2, 3, 4, 7), List(2, 3, 4, 8), List(2, 3, 4, 9),
+    val exp1 = List(List(2, 3, 4, 5), List(2, 3, 4, 6), List(2, 3, 4, 7), List(2, 3, 4, 8), List(2, 3, 4, 9),
       List(2, 3, 5, 6), List(2, 3, 5, 7), List(2, 3, 5, 8), List(2, 3, 5, 9), List(2, 3, 6, 7), List(2, 3, 6, 8),
       List(2, 3, 6, 9), List(2, 3, 7, 8), List(2, 3, 7, 9), List(2, 3, 8, 9), List(2, 4, 5, 6), List(2, 4, 5, 7),
       List(2, 4, 5, 8), List(2, 4, 5, 9), List(2, 4, 6, 7), List(2, 4, 6, 8), List(2, 4, 6, 9), List(2, 4, 7, 8),
@@ -29,8 +29,25 @@ class PermutationTest extends FunSuite with BeforeAndAfter with BeforeAndAfterAl
       List(3, 6, 8, 9), List(3, 7, 8, 9), List(4, 5, 6, 7), List(4, 5, 6, 8), List(4, 5, 6, 9), List(4, 5, 7, 8),
       List(4, 5, 7, 9), List(4, 5, 8, 9), List(4, 6, 7, 8), List(4, 6, 7, 9), List(4, 6, 8, 9), List(4, 7, 8, 9),
       List(5, 6, 7, 8), List(5, 6, 7, 9), List(5, 6, 8, 9), List(5, 7, 8, 9), List(6, 7, 8, 9))
-    val act = Permutation.iterator(4, 9, 2)
-    assert(act.toList == exp)
-    assert(act.toList == exp)
+    val act1 = Permutation.iterator(4, 9, 2)
+    assert(act1.toList == exp1)
+
+    val exp2 = List(List(1, 1, 1, 1), List(1, 1, 1, 2), List(1, 1, 1, 3), List(1, 1, 1, 4), List(1, 1, 1, 5),
+      List(1, 1, 1, 6), List(1, 1, 2, 2), List(1, 1, 2, 3), List(1, 1, 2, 4), List(1, 1, 2, 5), List(1, 1, 2, 6), List(1, 1, 3, 3), List(1, 1, 3, 4), List(1, 1, 3, 5), List(1, 1, 3, 6),
+      List(1, 1, 4, 4), List(1, 1, 4, 5), List(1, 1, 4, 6), List(1, 1, 5, 5), List(1, 1, 5, 6), List(1, 1, 6, 6),
+      List(1, 2, 2, 2), List(1, 2, 2, 3), List(1, 2, 2, 4), List(1, 2, 2, 5), List(1, 2, 2, 6), List(1, 2, 3, 3), List(1, 2, 3, 4), List(1, 2, 3, 5),
+      List(1, 2, 3, 6), List(1, 2, 4, 4), List(1, 2, 4, 5), List(1, 2, 4, 6), List(1, 2, 5, 5), List(1, 2, 5, 6),
+      List(1, 2, 6, 6), List(1, 3, 3, 3), List(1, 3, 3, 4), List(1, 3, 3, 5), List(1, 3, 3, 6), List(1, 3, 4, 4), List(1, 3, 4, 5), List(1, 3, 4, 6),
+      List(1, 3, 5, 5), List(1, 3, 5, 6), List(1, 3, 6, 6), List(1, 4, 4, 4), List(1, 4, 4, 5), List(1, 4, 4, 6),
+      List(1, 4, 5, 5), List(1, 4, 5, 6), List(1, 4, 6, 6), List(1, 5, 5, 5), List(1, 5, 5, 6), List(1, 5, 6, 6), List(1, 6, 6, 6),
+      List(2, 2, 2, 2), List(2, 2, 2, 3), List(2, 2, 2, 4), List(2, 2, 2, 5), List(2, 2, 2, 6), List(2, 2, 3, 3),
+      List(2, 2, 3, 4), List(2, 2, 3, 5), List(2, 2, 3, 6), List(2, 2, 4, 4), List(2, 2, 4, 5), List(2, 2, 4, 6), List(2, 2, 5, 5),
+      List(2, 2, 5, 6), List(2, 2, 6, 6), List(2, 3, 3, 3), List(2, 3, 3, 4), List(2, 3, 3, 5), List(2, 3, 3, 6), List(2, 3, 4, 4), List(2, 3, 4, 5), List(2, 3, 4, 6), List(2, 3, 5, 5), List(2, 3, 5, 6), List(2, 3, 6, 6), List(2, 4, 4, 4),
+      List(2, 4, 4, 5), List(2, 4, 4, 6), List(2, 4, 5, 5), List(2, 4, 5, 6), List(2, 4, 6, 6), List(2, 5, 5, 5), List(2, 5, 5, 6), List(2, 5, 6, 6), List(2, 6, 6, 6), List(3, 3, 3, 3), List(3, 3, 3, 4), List(3, 3, 3, 5), List(3, 3, 3, 6),
+      List(3, 3, 4, 4), List(3, 3, 4, 5), List(3, 3, 4, 6), List(3, 3, 5, 5), List(3, 3, 5, 6), List(3, 3, 6, 6), List(3, 4, 4, 4), List(3, 4, 4, 5), List(3, 4, 4, 6), List(3, 4, 5, 5), List(3, 4, 5, 6), List(3, 4, 6, 6), List(3, 5, 5, 5),
+      List(3, 5, 5, 6), List(3, 5, 6, 6), List(3, 6, 6, 6), List(4, 4, 4, 4), List(4, 4, 4, 5), List(4, 4, 4, 6), List(4, 4, 5, 5), List(4, 4, 5, 6), List(4, 4, 6, 6), List(4, 5, 5, 5), List(4, 5, 5, 6), List(4, 5, 6, 6), List(4, 6, 6, 6),
+      List(5, 5, 5, 5), List(5, 5, 5, 6), List(5, 5, 6, 6), List(5, 6, 6, 6), List(6, 6, 6, 6))
+    val act2 = Permutation.iterator(4, 6, 1, true)
+    assert(act2.toList == exp2)
   }
 }
